c31034ca620cb9531229d1943ee30200dbc50161
lagasani123 <166687381+lagasani123@users.noreply.github.com>
2024-04-18 15:22:19 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EligibleForMarriage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string gender = "female";
+            int age = 20;
+            if (gender == "male")
+            {
+                if (age >= 23)
+                {
+                    Console.WriteLine("Eligible");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("NotEligibleForMarriage");
+                    Console.ReadLine();
+                }
+            }
+
+            else if (gender == "female")
+            {
+                if (age >= 21)
+                {
+                    Console.WriteLine("Eligible");
+                    Console.ReadLine();
+                }
+
+                else
+                {
+                    Console.WriteLine("not Eligible");
+                    Console.ReadLine();
+                }
+            }
+
+            else if (gender == "transgender")
+            {
+                if (age >= 21)
+                {
+                    Console.WriteLine("Eligible");
+                    Console.ReadLine();
+                }
+
+                else
+                {
+                    Console.WriteLine("not Eligible");
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("not valid data");
+                Console.ReadLine();
+            }
+        }
+    }
+}
